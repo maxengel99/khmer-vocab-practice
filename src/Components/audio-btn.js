7589b95511b1bf1e883f6cc753f4647c6e49dcc9
Max Engel <maxengel99@gmail.com>
2019-09-02 19:31:21 +0700

    add btn

---
+++ src/Components/audio-btn.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import React, { Component } from "react";
+
+/*************************************************************************/
+
+export class AudioBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    );
+  }
+}
