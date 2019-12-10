e98447c6736a999a1466c2aa96838a2ec72b5c00
Max Engel <maxengel99@gmail.com>
2019-12-10 17:54:07 +0700

    update url for audio

--- src/Components/audio-btn.js
+++ src/Components/audio-btn.js
@@ -14,7 +14,7 @@ const categoryToFileNameMap = {
 function createUrl(category, value) {
   
   return category === "letter" ? 
-  `https://raw.githubusercontent.com/maxengel99/khmer-letter-anki/master/files/${value}.mp3` :
+  `https://raw.githubusercontent.com/maxengel99/khmer-letter-anki/master/files/letters/${value}.mp3` :
   `https://raw.githubusercontent.com/maxengel99/khmer-number-website/master/src/SoundFiles/${categoryToFileNameMap[category]}/${value}.mp3`;
 }
 
